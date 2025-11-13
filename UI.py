8316933c7752bd342db502274f6a5bd5be7f7873
1116925 <s2512542@bi.no>
2025-11-13 23:26:38 +0100

    A unit test with all the assertions passed for the argparser class

--- UI.py
+++ UI.py
@@ -6,6 +6,7 @@
 import argparse
 import os
 import sys
+from unittest.mock import patch
 
 class Input:
     '''
@@ -14,22 +15,31 @@ class Input:
     ---
     Main use is to extract the *args* property which holds the parsed arguments.
     '''
-    def __init__(self):
-        _parser = self.get_parser()
-        _args = _parser.parse_args()
-
-        # Handle clean_log logic
-        if _args.clean_log:
-            if not os.path.exists("training.log"):
-                print("No file to clean, a new file will be created.")
-            else:
-                print("Log file will be overwritten.")
+    def __init__(self, is_test=False):
+        self._is_test = is_test
+        self._parser = self._get_parser()
+        if not is_test:
+            self._args = self._parser.parse_args()
+            self._clean_log_warning()
+        else: # In test mode, args will be set manually via the setter
+            self._args = None
 
     @property
     def args(self): # Getter for parsed arguments
-        return self._args
+        return self._args   
+    @args.setter
+    def args(self, new_args): # EXCLUSIVE FOR TESTING Setter for forced parsed arguments. Used for testing with mocked sys.argv.
+        if not self._is_test:
+            raise AttributeError("Cannot set args when not in test mode.")
+        with patch.object(sys, 'argv', new_args):
+            self._args = self._parser.parse_args()
+            self._clean_log_warning()
+        
+    @property
+    def parser(self): # Getter for the argument parser
+        return self._parser
 
-    def positive_int(self, value):
+    def _positive_int(self, value):
         '''
         Does a sanity check on the "epochs" input to ensure it's a positive \n
         integer. raise ArgumentTypeError: if input is invalid
@@ -47,7 +57,7 @@ class Input:
             print(f"Warning: {ivalue} epochs is quite large, training may take a long time.", file=sys.stderr)
         return ivalue
 
-    def get_parser(self):
+    def _get_parser(self):
         parser = argparse.ArgumentParser(
             description="Train a Variational Autoencoder (VAE) with configurable options.",
             epilog='''Examples of usage:
@@ -68,7 +78,7 @@ class Input:
         # 2. Mandatory epochs
         parser.add_argument(
             "epochs",
-            type= self.positive_int,
+            type= self._positive_int,
             help="Number of training epochs (positive integer)."
         )
 
@@ -108,3 +118,11 @@ class Input:
         )
 
         return parser
+    
+    def _clean_log_warning(self):
+        # Handle clean_log logic
+        if self._args.clean_log:
+            if not os.path.exists("training.log"):
+                print("No file to clean, a new file will be created.")
+            else:
+                print("Log file will be overwritten.")
--- train_vae.py
+++ train_vae.py
@@ -4,6 +4,28 @@ This module is the main script for a program that generates an image using a VAE
 The following documentation is also provided when running the script from the terminal.
 
 ---
+Train a Variational Autoencoder (VAE) with configurable options.
+
+positional arguments:
+  {mnist_bw,mnist_color}
+                        Dataset to train the VAE on. Choices: "mnist_bw" (grayscale MNIST), "mnist_color" (colored MNIST).
+  epochs                Number of training epochs (positive integer).
+
+options:
+  -h, --help            show this help message and exit
+  --visualize_latent    Visualize the latent space after training.
+  --generate_form_prior
+                        Generate a new image from the prior distribution p(z).
+  --generate_form_posterior
+                        Generate a new image from the posterior distribution q(z|x).
+  --clean_log           Overwrite the current log file. If no log file exists, prints a message.
+  --custom_params CUSTOM_PARAMS
+                        Custom parameters for the VAE model IN DEVELOPMENT
+
+Examples of usage:
+    python train_vae.py mnist_bw 50 --visualize_latent
+    python train_vae.py mnist_color 200 --generate_form_prior --custom_params '{"lr":0.001,"beta":4}'
+    python train_vae.py mnist_bw 10 --clean_log
 '''
 
 ## --- Imports --- ##
